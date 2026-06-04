e0c9cd22cc13eed7b29c54681fa6514b40f42e54
Abdisa Awel <abdisaawel313@gmail.com>
2026-06-04 23:51:51 +0300

    books

--- views/librarian/categories.php
+++ views/librarian/categories.php
@@ -6,7 +6,7 @@
 require_once '../../config/config.php';
 require_once '../../includes/functions.php';
 
-requireRole('admin');
+requireRole('librarian');
 
 $pageTitle = 'Manage Categories';
 $category = new Category();
